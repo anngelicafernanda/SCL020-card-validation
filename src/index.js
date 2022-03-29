bd15303e44084d1861b188e782e4524111ec1080
Angelica Contreras <afernandac94@gmail.com>
2022-03-29 16:34:54 -0300

    funcion validadora integrada

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const numeroTarjeta = document.querySelector("#inputNumero");
+const esValida = document.querySelector(".esValida");
+
+numeroTarjeta.addEventListener("keyup", (e) => {
+  let valorInput = e.target.value;
+
+  numeroTarjeta.value = valorInput
+    .replace(/\s/g, "")
+    // Eliminar las letras
+    .replace(/\D/g, "")
+    // Ponemos espacio cada cuatro numeros
+    .replace(/([0-9]{4})/g, "$1 ")
+    // Elimina el ultimo espaciado
+    .trim();
+  console.log(valorInput);
+
+  esValida.textContent = validator.validar(valorInput);
+  //Muestra el true o false
+  numeroTarjeta.textContent = valorInput;
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,46 @@
+//import { inputRequiresLabel } from "htmlhint";
+
 const validator = {
-  // ...
+  validar: function (valorInput) {
+    //validar: (valorInput) => {
+
+    let tarjeta = valorInput;
+
+    let convierteTarjetaArray = tarjeta.split("");
+    //console.log(convierteTarjetaArray)
+
+    let numerotemporal = convierteTarjetaArray.filter((char) => char != " ");
+    //console.log(numerotemporal);
+
+    //let darReverseTarjeta = numerotemporal.reverse();
+    //console.log(darReverseTarjeta);
+
+    for (let i = 0; i < numerotemporal.length; i++) {
+      if (i % 2 === 0) {
+        let aux = numerotemporal[i] * 2;
+        if (aux >= 10) {
+          let aux2 = aux - 9;
+          numerotemporal[i] = aux2;
+        } else {
+          numerotemporal[i] = aux;
+        }
+      }
+
+      let suma = 0;
+      for (let i = 0; i < numerotemporal.length; i++) {
+        suma += parseInt(numerotemporal[i]);
+      }
+      console.log(suma);
+      //Debemos de comprobar si es multiplo de 10
+      if (suma % 10 === 0) {
+        console.log("tarjeta valida");
+        return "Tarjeta valida";
+      } else {
+        console.log("tarjeta invalida");
+        return "Tarjeta invalida";
+      }
+    }
+  },
 };
 
 export default validator;
