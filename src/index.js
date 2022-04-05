558821dbf61d5961eb898079ccf3f67244d25d83
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-04-05 11:37:21 -0400

    validacion nombre

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import validator from "./validator.js";
 
 const numeroTarjeta = document.querySelector("#inputNumero");
+const nombreTarjeta = document.querySelector("#inputNombre");
 const esValida = document.querySelector(".esValida");
 
+nombreTarjeta.addEventListener("keyup", (e) => {
+  let valorNombre = e.target.value;
+  nombreTarjeta.value = valorNombre
+  .replace(/[0-9]/g, "")
+});
+
+    
 numeroTarjeta.addEventListener("keyup", (e) => {
   let valorInput = e.target.value;
 
@@ -20,3 +28,6 @@ numeroTarjeta.addEventListener("keyup", (e) => {
   //Muestra el true o false
   numeroTarjeta.textContent = valorInput;
 });
+
+
+
