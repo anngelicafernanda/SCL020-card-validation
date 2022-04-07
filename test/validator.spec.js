9a55244d31a56a2ac675cf850f721756b8373e0c
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-04-07 16:17:13 -0400

    validacion con boton

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -11,12 +11,12 @@ describe('validator', () => {
       expect(typeof validator.isValid).toBe('function');
     });
 
-    it('debería retornar true para "4083952015263"', () => {
-      expect(validator.isValid('4083952015263')).toBe(true);
+    it('debería retornar true para "6011111111111117"', () => {
+      expect(validator.isValid('6011111111111117')).toBe(true);
     });
 
-    it('debería retornar true para "79927398713"', () => {
-      expect(validator.isValid('79927398713')).toBe(true);
+    it('debería retornar true para "5555555555554444"', () => {
+      expect(validator.isValid('5555555555554444')).toBe(true);
     });
 
     it('debería retornar false para "1234567890"', () => {
